082ac066a825f0cfc3200f9e9853f7e2012b82bf
Nagenthiran <nagenthirannagarajh@gmail.com>
2023-03-29 12:27:57 +0530

    The packages that showeda as error were solved

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pickle
+import json
 # import the libraries from flask that we going to use here 
-from flask import Flask, reuquest, app , jsonify, url_for,render_template 
+from flask import Flask,request,app,jsonify,url_for,render_template
 # we will import some of the main libraries that we going to use here 
 import numpy as np 
 import pandas as pd
@@ -9,14 +10,14 @@ import pandas as pd
 # define the flask app
 app = Flask(__name__)
 # load our model
-regmodel = pickle.load(open('regmode.pkl','rb'))
-scaler =pickle.load(open('scalling.pkl','rb'))
+regmodel = pickle.load(open("regmodel.pkl","rb"))
+scaler =pickle.load(open("scalling.pkl","rb"))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# we here creating a aapi to run the model, get the prediction and retur the predicted value 
+# we here creating a api to run the model, get the prediction and retur the predicted value 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
     # just check about github copilot that helps to auto generate the code when we start the code 
